d2961bb3ffc583974e87dc4bc4ee71aba12e7f89
Klaus Post <klauspost@gmail.com>
2018-08-10 13:57:47 +0200

    Add code, etc

---
+++ ep02/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"image"
+	_ "image/png"
+	"math"
+	"math/bits"
+
+	_ "github.com/klauspost/gad/ep02/data" // Load data.
+	"github.com/klauspost/gfx"
+)
+
+// Generates binary data.
+// To install go-bindata, do: go get -u github.com/jteeuwen/go-bindata/...
+//
+//go:generate go-bindata -ignore=\.go\z -pkg=data -o ./data/data.go ./data/...
+
+const (
+	renderWidth  = 640
+	renderHeight = 360
+)
+
+func main() {
+	fx := newFx("data/ballet-egon-256.png")
+	gfx.Run(func() { gfx.RunTimed(fx) })
+}
+
+type fx struct {
+	img        *image.Paletted
+	draw       *image.Paletted
+	logW, logH uint
+	lines      [][]byte
+}
+
+func newFx(file string) *fx {
+	var fx fx
+
+	// Load picture
+	img, err := gfx.LoadPalPicture(file)
+	if err != nil {
+		panic(err)
+	}
+	fx.img = img
+
+	// Calculate the with in log(2)
+	fx.logW = uint(bits.Len32(uint32(img.Rect.Dx()))) - 1
+	fx.logH = uint(bits.Len32(uint32(img.Rect.Dy()))) - 1
+
+	// Ensure that the width and height are actually powers of two
+	if img.Rect.Dx() != 1<<fx.logW {
+		panic("image " + file + " width is not power of two.")
+	}
+	if img.Rect.Dy() != 1<<fx.logH {
+		panic("image " + file + " width is not power of two.")
+	}
+
+	// Create our draw buffer
+	fx.draw = image.NewPaletted(image.Rect(0, 0, renderWidth, renderHeight), img.Palette)
+
+	// Store each line as a slice in a slice.
+	fx.lines = make([][]byte, fx.draw.Rect.Dy())
+	for y := range fx.lines {
+		fx.lines[y] = fx.draw.Pix[y*fx.draw.Stride : y*fx.draw.Stride+fx.draw.Rect.Dx()]
+	}
+	return &fx
+}
+
+// Render the effect at time t.
+func (fx *fx) Render(t float64) image.Image {
+	const (
+		DecimalPointLog = 16
+		DecimalMul      = 1 << DecimalPointLog
+	)
+	xMask := (1 << fx.logW) - 1
+	yMask := (1 << fx.logH) - 1
+	// tt is our reverse zoom as 16.16 fixed point
+	tt := int((math.Pow(t+0.05, 2) * 10) * DecimalMul)
+	logY := fx.logW
+
+	// Center of zoom (screen space)
+	centerX, centerY := renderWidth/2, renderHeight/2
+	// Store the reverse transformation for the center of screen.
+	x0 := -tt * centerX
+	y0 := -tt * centerY
+
+	// Center on texture (texture space)
+	texCenterX, texCenterY := 173, 106
+	x0 += texCenterX * DecimalMul
+	y0 += texCenterY * DecimalMul
+
+	for y, line := range fx.lines {
+		for x := range line {
+			srcX := ((x0 + x*tt) >> DecimalPointLog) & xMask
+			srcY := ((y0 + y*tt) >> DecimalPointLog) & yMask
+			line[x] = fx.img.Pix[srcX+(srcY<<logY)]
+		}
+	}
+	return fx.draw
+}
